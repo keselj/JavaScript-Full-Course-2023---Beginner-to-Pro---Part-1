48d3ab39895d7679645a01f45d2f49f602dabb2c
keselj <keselj.mil@gmail.com>
2023-06-28 15:08:14 +0200

    Adding from part 11

--- scripts/11-Exerises.js
+++ scripts/11-Exerises.js
@@ -1,54 +1,54 @@
 // Exercise 11a
 
-const nums = [10,20,30];
+const nums = [10, 20, 30];
 nums[2] = 99;
-console.log(nums)
+console.log(nums);
 
-// Exercise 11b 
+// Exercise 11b
 
 function getLatsValue(array) {
-  return array[array.length -1];
+  return array[array.length - 1];
 }
 
-console.log(getLatsValue([1,20,22,24,5]));
+console.log(getLatsValue([1, 20, 22, 24, 5]));
 
 // Exercise 11c
 
 function arraySwap(array1) {
-  let firstValue = array1[array1.length -1];
+  let firstValue = array1[array1.length - 1];
   let lastValue = array1[0];
   array1[0] = lastValue;
-  array1[array1.length -1] = firstValue;
+  array1[array1.length - 1] = firstValue;
   return array1;
 }
 
-console.log(arraySwap([1,20,22,24,5]));
-console.log(arraySwap(['hi', 'hello', 'good']));
+console.log(arraySwap([1, 20, 22, 24, 5]));
+console.log(arraySwap(["hi", "hello", "good"]));
 
 // Exercise 11d
 
-for(let i = 0; i < 11; i+= 2) {
+for (let i = 0; i < 11; i += 2) {
   console.log(i);
 }
 
 // Exercise 11e
 
-for(let i = 5; i >= 0; i--) {
-  console.log(i)
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
 }
 
 // Exercise 11f
 
 let i = 0;
 
-while(i < 11) {
-  console.log(i)
+while (i < 11) {
+  console.log(i);
   i += 2;
 }
 
 let j = 5;
 
-while(j >= 0) {
+while (j >= 0) {
   console.log(j);
   j--;
 }
@@ -56,81 +56,78 @@ while(j >= 0) {
 // Exercise 11g and 11h
 
 function addOneToArray(array2) {
-  for(let i = 0; i < array2.length; i++) {
-    array2[i]++
+  for (let i = 0; i < array2.length; i++) {
+    array2[i]++;
   }
-  return array2
+  return array2;
 }
 
-console.log(addOneToArray([1,2,3]));
+console.log(addOneToArray([1, 2, 3]));
 
 // Exercise 11i
 
 function addArrays(array3, num) {
-  for(let i = 0; i < array3.length; i++){
+  for (let i = 0; i < array3.length; i++) {
     array3[i] += num;
   }
 
   return array3;
 }
 
-console.log(addArrays([1,2,3],2));
-console.log(addArrays([1,2,3],3));
-console.log(addArrays([-2,-1,0,99],2));
+console.log(addArrays([1, 2, 3], 2));
+console.log(addArrays([1, 2, 3], 3));
+console.log(addArrays([-2, -1, 0, 99], 2));
 
 // Exercise 11j
 
 function addArraysTogether(array4, array5) {
-  let array4And5 = []
-  for(let i = 0; i < array4.length; i++){
+  let array4And5 = [];
+  for (let i = 0; i < array4.length; i++) {
     array4And5.push(array4[i] + array5[i]);
   }
   return array4And5;
 }
 
-console.log(addArraysTogether([1,1,2], [1,1,3]));
+console.log(addArraysTogether([1, 1, 2], [1, 1, 3]));
 
-console.log(addArraysTogether([1,2,3], [4,5,6]));
-
+console.log(addArraysTogether([1, 2, 3], [4, 5, 6]));
 
 // Exercise 11k
 
 function countPositive(nums) {
   let count = 0;
-  for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] > 0) {
-      count++
+      count++;
     }
   }
-  return count
+  return count;
 }
 
-console.log(countPositive([1,-3,5]));
-console.log(countPositive([-2,3,-5,7,10]));
-
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
 
 // Exercise 11l and 11m
 
 function minMax(nums) {
   let object1 = {
     min: undefined,
-    max: undefined
-  }
-  if(nums.length === 0){
+    max: undefined,
+  };
+  if (nums.length === 0) {
     object1.min = null;
     object1.max = null;
-  }else {
+  } else {
     object1.max = Math.max(...nums);
     object1.min = Math.min(...nums);
   }
   return object1;
 }
 
-console.log(minMax([1,-3,5]));
-console.log(minMax([-2,3,-5,7,10]));
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, 10]));
 console.log(minMax([]));
 console.log(minMax([3]));
-
 
 // Exercise 11n
 
@@ -138,47 +135,25 @@ function countWords(words) {
   let object2 = {};
   for (let i = 0; i < words.length; i++) {
     const word = words[i];
-    if(!object2[word]){
+    if (!object2[word]) {
       object2[word] = 1;
     } else {
       object2[word]++;
     }
-    
   }
-  return object2
-
+  return object2;
 }
 
-console.log(countWords(['apple', 'grape', 'apple', 'apple']));
+console.log(countWords(["apple", "grape", "apple", "apple"]));
 
 // Exercise 11o and 11p
 
 function arrayOfStrings(array6) {
-  let result = '';
-  for (let i = 0; i < array6.length; i++){
-    if (array6[i] === 'search') {
+  let result = "";
+  for (let i = 0; i < array6.length; i++) {
+    if (array6[i] === "search") {
       result = i;
-      break
-    } else {
-      result = -1
-    }
-  }
-  return result;
-}
-
-console.log(arrayOfStrings(['hello', 'world', 'search', 'good']));
-console.log(arrayOfStrings(['not', 'found']));
-console.log(arrayOfStrings(['hello', 'world', 'search', 'good', 'search']));
-
-
-// Exercise 11q
-
-function findIndex(array, word) {
-  let result = 0;
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] === word) {
-      result = i;
-      break
+      break;
     } else {
       result = -1;
     }
@@ -186,24 +161,43 @@ function findIndex(array, word) {
   return result;
 }
 
-console.log(findIndex(['green', 'red', 'blue', 'red'], 'red'));
-console.log(findIndex(['green', 'red', 'blue', 'red'], 'yellow'));
+console.log(arrayOfStrings(["hello", "world", "search", "good"]));
+console.log(arrayOfStrings(["not", "found"]));
+console.log(arrayOfStrings(["hello", "world", "search", "good", "search"]));
+
+// Exercise 11q
+
+function findIndex(array, word) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === word) {
+      result = i;
+      break;
+    } else {
+      result = -1;
+    }
+  }
+  return result;
+}
+
+console.log(findIndex(["green", "red", "blue", "red"], "red"));
+console.log(findIndex(["green", "red", "blue", "red"], "yellow"));
 
 // Exercise 11r
 
 function removeEgg(foods) {
   let newFoods = [];
-  for(let i = 0; i < foods.length; i++) {
-    if(foods[i] === 'egg') {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "egg") {
       continue;
     } else {
       newFoods.push(foods[i]);
     }
   }
-  return newFoods
+  return newFoods;
 }
 
-console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+console.log(removeEgg(["egg", "apple", "egg", "egg", "ham"]));
 
 // Exercise 11s and 11t
 
@@ -211,9 +205,9 @@ function removeEgg2(foods) {
   let reversFoods = foods.reverse();
   let newFoods = [];
   let eggCount = 0;
-  for(let i = 0; i < foods.length; i++) {
-    if(reversFoods[i] === 'egg' && eggCount < 2) {
-      eggCount ++;
+  for (let i = 0; i < foods.length; i++) {
+    if (reversFoods[i] === "egg" && eggCount < 2) {
+      eggCount++;
       continue;
     } else {
       newFoods.push(reversFoods[i]);
@@ -222,7 +216,7 @@ function removeEgg2(foods) {
   return newFoods.reverse();
 }
 
-console.log(removeEgg2(['egg', 'apple', 'egg', 'egg', 'ham']));
+console.log(removeEgg2(["egg", "apple", "egg", "egg", "ham"]));
 
 // Exercise 11u
 
@@ -230,9 +224,9 @@ function removeEgg2(foods) {
   let reversFoods = foods.slice().reverse();
   let newFoods = [];
   let eggCount = 0;
-  for(let i = 0; i < foods.length; i++) {
-    if(reversFoods[i] === 'egg' && eggCount < 2) {
-      eggCount ++;
+  for (let i = 0; i < foods.length; i++) {
+    if (reversFoods[i] === "egg" && eggCount < 2) {
+      eggCount++;
       continue;
     } else {
       newFoods.push(reversFoods[i]);
@@ -241,42 +235,48 @@ function removeEgg2(foods) {
   return newFoods.reverse();
 }
 
-console.log(removeEgg2(['egg', 'apple', 'egg', 'egg', 'ham']));
+console.log(removeEgg2(["egg", "apple", "egg", "egg", "ham"]));
 
 // Exercise 11v
 
 let u = 0;
 
 while (u <= 20) {
-  if(u % 3 === 0 && u % 5 === 0) {
-    console.log('FizzBuzz');
-  } else if(u % 3 === 0) {
-    console.log('Fizz');
-  } else if(u % 5 === 0) {
-    console.log('Buzz')
-  } else{
-    console.log(u)
+  if (u % 3 === 0 && u % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (u % 3 === 0) {
+    console.log("Fizz");
+  } else if (u % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(u);
   }
   u++;
 }
 
-
-
 // Exercise 11w
 
-/*function findIndex(array, word) {
-  let result = 0;
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] === word) {
-      result = i;
-      break
-    } else {
-      result = -1;
+function findIndex1(array, word) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === word) {
+      return i;
     }
   }
-  return result;
+  return -1;
 }
 
-console.log(findIndex(['green', 'red', 'blue', 'red'], 'red'));
-console.log(findIndex(['green', 'red', 'blue', 'red'], 'yellow'));
-*/
+function unique(array8) {
+  const returnArray = [];
+
+  for (let j = 0; j < array8.length; j++) {
+    const word = array8[j];
+
+    if (findIndex1(returnArray, word) === -1) {
+      returnArray.push(word);
+    }
+  }
+  return returnArray;
+}
+
+console.log(unique(["green", "red", "blue", "red"]));
+console.log(unique(["red", "green", "green", "red"]));
---
+++ scripts/11-todo-list.js
@@ -0,0 +1,54 @@
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2023-06-28",
+  },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div> 
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  // This is input element, but without vaule
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-inpit");
+  const dueDate = dateInputElement.value;
+
+  // This is input element with value added to it in Chrome
+
+  todoList.push({
+    name,
+    dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
